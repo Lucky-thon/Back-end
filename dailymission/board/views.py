f42ed407302043a0da4f3e7034d105656ed4d5d7
smiinii <sm020320@naver.com>
2024-10-31 22:08:39 +0900

    로그 확인 추가

--- dailymission/board/views.py
+++ dailymission/board/views.py
@@ -6,6 +6,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import MissionSuccessPost
 from .serializers import MissionSuccessPostSerializer
+import logging
 
 def mission_success_list(request):
     posts = MissionSuccessPost.objects.all()
