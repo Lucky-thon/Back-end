aad75201020a5c136b63466b97d19593a6404b7f
smiinii <sm020320@naver.com>
2024-11-01 02:47:37 +0900

    인원 모집 댓글 생성 변경

--- dailymission/board/serializers.py
+++ dailymission/board/serializers.py
@@ -22,9 +22,11 @@ class RecruitmentPostSerializer(serializers.ModelSerializer):
 class RecruitmentCommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = RecruitmentComment
-        fields = ['id', 'post', 'author', 'content', 'created_at']
+        fields = '__all__'
 
     def create(self, validated_data):
-        # 현재 사용자 설정
+        # 현재 사용자를 작성자로 설정
         validated_data['author'] = self.context['request'].user
-        return super().create(validated_data)
+        # 부모 클래스의 create 메서드 호출
+        return RecruitmentComment.objects.create(**validated_data)
+
